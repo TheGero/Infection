dc8bcadc1432b00bc01d0200ae91b4dde7e50a95
Gero <kacper.lesnianski@wp.pl>
2019-05-22 20:31:40 +0200

    added implementation of class Main

--- src/infection/VirusData.java
+++ src/infection/VirusData.java
@@ -1,4 +1,10 @@
 package infection;
 
 public class VirusData {
+    public int spreadRange;
+    public int spreadChance;
+    public int mutationChance;
+    public int lethality;
+    public int resistanceToTreatment;
+    //TODO: add symptoms
 }
