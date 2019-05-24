474228847b21b7d57fa9a10f6b3816ce9275ce1f
Gero <kacper.lesnianski@wp.pl>
2019-05-24 21:43:57 +0200

    Finished implementation of VirusData, Added impl. of Symptom

--- src/main/java/Symptom.java
+++ src/main/java/Symptom.java
@@ -1,2 +1,6 @@
 public class Symptom {
+    public int visibility;
+    Symptom(int visibility){
+        this.visibility = visibility;
+    }
 }
--- src/main/java/VirusData.java
+++ src/main/java/VirusData.java
@@ -1,8 +1,16 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class VirusData {
     public int spreadRange;
     public int spreadChance;
     public int mutationChance;
     public int lethality;
     public int resistanceToTreatment;
-    //TODO: add symptoms
+    List<Symptom> symptoms;
+
+    VirusData(){
+        symptoms = new ArrayList<>();
+    }
 }
