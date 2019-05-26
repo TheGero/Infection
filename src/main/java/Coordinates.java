bca60896c340ccdfe68f780c672362d587e26f8c
PATRYK PŁÓCIENNIK <patplo1999@wp.pl>
2019-05-26 16:50:29 +0200

    wiadomość

--- src/main/java/Coordinates.java
+++ src/main/java/Coordinates.java
@@ -1,2 +1,23 @@
 public class Coordinates {
+    protected int X;
+    protected int Y;
+
+    public Coordinates(int X, int Y) {
+    }
+
+    public void setX(int X) {
+        this.X = X;
+    }
+
+    public void setY(int Y) {
+        this.Y = Y;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public int getY() {
+        return Y;
+    }
 }
