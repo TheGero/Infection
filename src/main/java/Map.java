33a3a20d8306242d7ccfa8bf4d93bda6c0941138
PATRYK PŁÓCIENNIK <patplo1999@wp.pl>
2019-05-28 18:10:16 +0200

    Started implementing Map class

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,2 +1,22 @@
-public class Map {
+public class Map
+{
+    private int size;
+    private IHuman[] humans;
+    private HumanCreator hFactory;
+
+    public Map(int size, int humanCount,int doctorCount)
+    {
+        this.size=size;
+    }
+    public void update() {/*TODO:Implement*/}
+
+    // public IHuman[] getHumansInRange(Coordinates coordinates,int range) {/*TODO:Implement*/}
+
+    // public int getHumansLeft();
+    // public int getDoctorsLeft();
+    // public int getInfectedLeft();
+    public int getSize(){return size;}
+    //public int getMutationCount();
+    private void startInfection(){/*TODO:Implement*/}
+
 }
