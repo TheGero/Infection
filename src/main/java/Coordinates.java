6133bdd1891f07adf3aedd9a45754f52afaf26fc
PATRYK PŁÓCIENNIK <patplo1999@wp.pl>
2019-05-31 13:58:29 +0200

    Documentation of Coordinates

--- src/main/java/Coordinates.java
+++ src/main/java/Coordinates.java
@@ -1,30 +1,61 @@
+/**
+ * Coordinates stores X and Y Map coordinates of Human and Doctor objects
+ *
+ * @author Kacper Leśniański , Patryk Płóciennik
+ * @version 1.0
+ */
 public class Coordinates
 {
     private int X;
     private int Y;
 
+    /**
+     * Set given values.
+     *
+     * @param X - X Coordinate.
+     * @param Y - Y Coordinate.
+     */
     public Coordinates(int X, int Y) {
         this.X = X;
         this.Y = Y;
     }
 
+    /**
+     * Set default values.
+     */
     public Coordinates() {
         this(0, 0);
     }
 
-    public void setX(int X) {
-        this.X = X;
-    }
-
-    public void setY(int Y) {
-        this.Y = Y;
-    }
-
+    /**
+     * @return X Coordinate value.
+     */
     public int getX() {
         return X;
     }
 
+    /**
+     * Sets X Coordinate value.
+     *
+     * @param X - X Coordinate of Human stored in the Map.
+     */
+    public void setX(int X) {
+        this.X = X;
+    }
+
+    /**
+     * @return Y Coordinate value.
+     */
     public int getY() {
         return Y;
+    }
+
+    /**
+     * Sets Y Coordinate value.
+     *
+     * @param Y - Y Coordinate of Human stored in the Map.
+     */
+    public void setY(int Y) {
+        this.Y = Y;
     }
 }
