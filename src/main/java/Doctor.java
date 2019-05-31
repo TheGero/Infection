994fd3accc8d80b8fcb2153132dbbc6f0397cb0f
Kacper Leśniański <kacper.lesnianski@wp.pl>
2019-05-31 13:54:36 +0200

    Documentation of Doctor

--- src/main/java/Doctor.java
+++ src/main/java/Doctor.java
@@ -1,10 +1,23 @@
 import java.util.List;
-
+/**
+ * Class representing a Doctor
+ * @author Kacper Leśniański, Patryk Płóciennik
+ * @version 1.0
+ */
 public class Doctor extends AbstractHuman {
+    /**
+     * Constructor (calls AbstractHuman constructor)
+     * @param parentMap Reference to Map storing Doctor
+     */
     public Doctor(Map parentMap) {
         super(parentMap);
     }
 
+    /**
+     * Method called on each step of the simulation.
+     * Controls all actions of the Doctor: moving and healing
+     * If Doctor is infected, updates Virus
+     */
     @Override
     public void update() {
         if (!isAlive()) return;
@@ -14,15 +27,26 @@ public class Doctor extends AbstractHuman {
             if (h.hasVisibleSymptoms()) heal(h);
         }
 
+        move();
+
         if (isInfected())
             getVirus().update();
     }
 
+    /**
+     * This method is required to discriminate between a Doctor and a Human without using instanceof()
+     * (Used in class Map)
+     * @return true if is a Doctor.
+     */
     @Override
     public boolean isDoctor() {
         return true;
     }
 
+    /**
+     * Attempt to cure patient from infection
+     * @param patient reference to patient
+     */
     private void heal(IHuman patient) {
         int r = RandomNumberGenerator.getIntegerFromRange(0, 100);
         int chanceToHeal = 100 - patient.getVirus().getVirusData().resistanceToTreatment;
