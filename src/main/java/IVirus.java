284a65c56d1b6ccab64a6db7b570ef70bff39966
PATRYK PŁÓCIENNIK <patplo1999@wp.pl>
2019-05-31 14:21:53 +0200

    Documentation of IVirus

--- src/main/java/IVirus.java
+++ src/main/java/IVirus.java
@@ -1,9 +1,28 @@
+/**
+ * An interface Virus objects.
+ *
+ * @author Kacper Leśniański, Patryk Płóciennik
+ * @version 1.0
+ */
 public interface IVirus {
+    /**
+     * Update Virus.
+     */
     void update();
 
+    /**
+     * @return SymptomsVisibility value.
+     */
     int getSymptomsVisibility();
 
+    /**
+     * @return Virus's data.
+     */
     VirusData getVirusData();
 
+    /**
+     *
+     * @return true if Virus has mutaded.
+     */
     boolean hasMutatedLastStep();
 }
