a73d15879f80686268f020a6dccdbc14e15a3adf
Kacper Leśniański <kacper.lesnianski@wp.pl>
2019-06-07 13:19:12 +0200

    Tests

--- src/test/java/HumanCreatorTest.java
+++ src/test/java/HumanCreatorTest.java
@@ -9,15 +9,36 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class HumanCreatorTest {
 
+    HumanCreator hc;
+
+    HumanCreatorTest(){
+        hc = new HumanCreator();
+    }
+
     @Test
+    /**
+     * Testing if createHuman method creates a Human
+     */
     void createHuman() {
+        IHuman h = hc.createHuman(null);
+        assertTrue(h instanceof Human);
     }
 
     @Test
+    /**
+     * Testing if createDoctor method creates a Doctor
+     */
     void createDoctor() {
+        IHuman h = hc.createDoctor(null);
+        assertTrue(h instanceof Doctor);
     }
 
     @Test
+    /**
+     * Testing if createInfected method creates an infected Human
+     */
     void createInfected() {
+        IHuman h = hc.createInfected(null, new VirusData());
+        assertTrue(h instanceof Human && h.isInfected());
     }
 }
--- src/test/java/VirusTest.java
+++ src/test/java/VirusTest.java
@@ -13,6 +13,21 @@ class VirusTest {
      * Testing if getVirusData returns correct value
      */
     void getVirusData() {
+        VirusData d = new VirusData();
+        d.spreadChance = 2;
+        d.spreadRange = 1;
+        d.mutationChance = 3;
+        d.lethality = 7;
+        d.resistanceToTreatment = 8;
+
+        Virus v = new Virus(null, d);
+
+        assertEquals(v.getVirusData().spreadChance, 2);
+        assertEquals(v.getVirusData().spreadRange, 1);
+        assertEquals(v.getVirusData().mutationChance, 3);
+        assertEquals(v.getVirusData().lethality, 7);
+        assertEquals(v.getVirusData().resistanceToTreatment, 8);
+        assertEquals(v.getVirusData().symptoms.size(), 0);
     }
 
     @Test
@@ -20,12 +35,27 @@ class VirusTest {
      * Testing if getSymptomsVisibility returns correct value
      */
     void getSymptomsVisibility() {
+        VirusData d = new VirusData();
+        d.symptoms.add(new Symptom(0));
+
+        Virus v = new Virus (null, d);
+        assertEquals(v.getSymptomsVisibility(), 0);
+        d.symptoms.add(new Symptom(30));
+        d.symptoms.add(new Symptom(100));
+        v = new Virus(null, d);
+        assertEquals(v.getSymptomsVisibility(), 130);
     }
 
     @Test
     /**
-     * 
+     *  Checks if hasMutatedLastStep works
      */
     void hasMutatedLastStep() {
+        VirusData d = new VirusData();
+        d.mutationChance = 100;
+        Virus v = new Virus(null,d);
+        assertFalse(v.hasMutatedLastStep());
+        v.update();
+        assertTrue(v.hasMutatedLastStep());
     }
 }
